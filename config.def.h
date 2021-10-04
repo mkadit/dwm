7ddece299727e02faf731fc08c99ccefd17f12e9
mkadit <mkaditya00@gmail.com>
2021-10-04 15:51:21 +0700

    Added vanitygaps

--- config.def.h
+++ config.def.h
@@ -3,13 +3,18 @@
 /* appearance */
 static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:pixelsize=14:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:size=10";
 static char normbgcolor[]           = "#2e3440";
 static char normbordercolor[]       = "#434c5e";
-static char normfgcolor[]           = "#d8dee9";
+static char normfgcolor[]           = "#eca";
 static char selfgcolor[]            = "#2e3440"; 
 static char selbordercolor[]        = "#81a1c1"; /*770000*/
 static char selbgcolor[]            = "#81a1c1"; /*ffc922*/
@@ -44,10 +49,24 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
+#define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
+#include "vanitygaps.c"
+
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "[M]",      monocle },
+	{ "[@]",      spiral },
+	{ "[\\]",     dwindle },
+	{ "D[]",      deck },
+	{ "TTT",      bstack },
+	{ "===",      bstackhoriz },
+	{ "HHH",      grid },
+	{ "###",      nrowgrid },
+	{ "---",      horizgrid },
+	{ ":::",      gaplessgrid },
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ NULL,       NULL },
 };
@@ -125,6 +144,25 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8, 7)
 	TAGKEYS(                        XK_9, 8)
 	{ MODKEY|ControlMask|ShiftMask,       XK_q,         quit,             {1} },
+
+	{ MODKEY|TERMMOD,              XK_u,      incrgaps,       {.i = +1 } },
+	{ MODKEY|TERMMOD|ShiftMask,    XK_u,      incrgaps,       {.i = -1 } },
+	{ MODKEY|TERMMOD,              XK_i,      incrigaps,      {.i = +1 } },
+	{ MODKEY|TERMMOD|ShiftMask,    XK_i,      incrigaps,      {.i = -1 } },
+	{ MODKEY|TERMMOD,              XK_o,      incrogaps,      {.i = +1 } },
+	{ MODKEY|TERMMOD|ShiftMask,    XK_o,      incrogaps,      {.i = -1 } },
+	// Start, not deleted due to not getting warnings when making
+	{ MODKEY|TERMMOD,              XK_0,      incrihgaps,     {.i = +1 } },
+	{ MODKEY|TERMMOD|ShiftMask,    XK_0,      incrihgaps,     {.i = -1 } },
+	{ MODKEY|TERMMOD,              XK_0,      incrivgaps,     {.i = +1 } },
+	{ MODKEY|TERMMOD|ShiftMask,    XK_0,      incrivgaps,     {.i = -1 } },
+	{ MODKEY|TERMMOD,              XK_0,      incrohgaps,     {.i = +1 } },
+	{ MODKEY|TERMMOD|ShiftMask,    XK_0,      incrohgaps,     {.i = -1 } },
+	{ MODKEY|TERMMOD,              XK_0,      incrovgaps,     {.i = +1 } },
+	{ MODKEY|TERMMOD|ShiftMask,    XK_0,      incrovgaps,     {.i = -1 } },
+	//End
+	{ MODKEY|TERMMOD,              XK_0,      togglegaps,     {0} },
+	{ MODKEY|TERMMOD|ShiftMask,    XK_0,      defaultgaps,    {0} },
 };
 
 /* button definitions */
